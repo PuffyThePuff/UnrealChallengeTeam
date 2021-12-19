851658281882969949100186530dc180e89b0381
PuffyThePuff <79883134+PuffyThePuff@users.noreply.github.com>
2021-12-19 22:45:46 +0800

    Initial commit

---
+++ Source/UnrealChallengeTeam/UnrealChallengeTeamGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealChallengeTeamGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AUnrealChallengeTeamGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AUnrealChallengeTeamGameMode();
+};
+
+
+
