b89d44a9b7c85b9d5413c86713c802e7b9ebf6bd
DenziCs <nathaniel_francis_filoteo@dlsu.edu.ph>
2022-02-02 20:16:41 +0800

    Added spawn behavior.

--- Source/UnrealChallengeTeam/ProjectileUpgradeBehavior.cpp
+++ Source/UnrealChallengeTeam/ProjectileUpgradeBehavior.cpp
@@ -32,8 +32,26 @@ void UProjectileUpgradeBehavior::SpawnUpgrade()
 	int itemToSpawn = (int) FMath::FRandRange(0.f, 3.f);
 	switch (itemToSpawn) {
 	case 0:
+		FActorSpawnParameters spawnParameters;
+		spawnParameters.Template = upgradeItem1;
+		spawnParameters.Owner = this->GetOwner()->GetOwner();
+
+		AActor* upgradeItem = this->GetOwner()->GetWorld()->SpawnActor<AActor>(upgradeItem1->GetClass(), spawnParameters);
+		upgradeItem->AttachToActor(this->GetOwner()->GetOwner(), FAttachmentTransformRules::KeepRelativeTransform);
+		upgradeItem->SetActorHiddenInGame(false);
+		upgradeItem->SetActorLocation(this->GetOwner()->GetActorLocation());
+		upgradeItem->SetActorRotation(this->GetOwner()->GetActorRotation());
 		break;
 	case 1:
+		FActorSpawnParameters spawnParameters;
+		spawnParameters.Template = upgradeItem2;
+		spawnParameters.Owner = this->GetOwner()->GetOwner();
+
+		AActor* upgradeItem = this->GetOwner()->GetWorld()->SpawnActor<AActor>(upgradeItem2->GetClass(), spawnParameters);
+		upgradeItem->AttachToActor(this->GetOwner()->GetOwner(), FAttachmentTransformRules::KeepRelativeTransform);
+		upgradeItem->SetActorHiddenInGame(false);
+		upgradeItem->SetActorLocation(this->GetOwner()->GetActorLocation());
+		upgradeItem->SetActorRotation(this->GetOwner()->GetActorRotation());
 		break;
 	}
 }
